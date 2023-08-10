7ea738e3237f4877609a5dd5f4141be4603a202e
tavaresflavia <altavares.flavia@gmail.com>
2023-08-09 21:15:02 -0400

    completed index.js code

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const cors = require('cors');
+const videosRouter = require('./routes/videos')
+
 
 require('dotenv').config()
 
 const {PORT, BACKEND_URL } = process.env
 
-app.listen(PORT, console.log(`listening port ${PORT}`))
+app.use(cors({
+    origin: BACKEND_URL
+}));
 
+app.use(express.json());
+
+app.use(express.static('./public'))
+
+app.use('/videos', videosRouter);
+
+
+app.listen(PORT, console.log(`listening port ${PORT}`));
